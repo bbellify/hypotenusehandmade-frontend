29f1b761c4b2369d74d03098a018cc5fe0e1de4c
Brian Bellinger <brianpatrickbellinger@gmail.com>
2022-01-22 17:30:39 -0800

    wip

--- src/components/NavSide.js
+++ src/components/NavSide.js
@@ -10,16 +10,16 @@ function NavSide(props) {
             <Container className='d-flex-column ps-3 my-5'>
                 <p className='fs-4'>Handmades</p>
                     <div className='d-flex flex-column'>
-                        <NavLink to='/test' className='my-2'>tops</NavLink>
-                        <NavLink to='/bottoms' className='my-2'>bottoms</NavLink>
-                        <NavLink to='/dresses' className='my-2'>dresses</NavLink>
-                        <NavLink to='/hats-hankies' className='my-2'>hats/hankies</NavLink>
+                        <NavLink to='/test' className='my-2'>Tops</NavLink>
+                        <NavLink to='/bottoms' className='my-2'>Bottoms</NavLink>
+                        <NavLink to='/dresses' className='my-2'>Dresses</NavLink>
+                        <NavLink to='/hats-hankies' className='my-2'>Hats/Hankies</NavLink>
                     </div>
                 <p className='fs-4 mt-4'>Categories</p>
                 <div className='d-flex flex-column'>
-                        <NavLink to='/kids' className='my-2'>kids</NavLink>
-                        <NavLink to='/women' className='my-2'>women</NavLink>
-                        <NavLink to='/men' className='my-2'>men</NavLink>
+                        <NavLink to='/kids' className='my-2'>Kids</NavLink>
+                        <NavLink to='/women' className='my-2'>Women</NavLink>
+                        <NavLink to='/men' className='my-2'>Men</NavLink>
                     </div>
             </Container>
         </div>
--- src/components/contents/Test.js
+++ src/components/contents/Test.js
@@ -9,49 +9,49 @@ function Home(props) {
 
             
                 <div className='col-3 position-relative img_mask'>
-                    <p className='img_description top-50 start-50 translate-middle fw-bold' >babies</p>
+                    <p className='img_text top-50 start-50 translate-middle fw-bold' >babies</p>
                     <img src={'https://images.pexels.com/photos/4964290/pexels-photo-4964290.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'} className='p-1 w-100 img_img' alt='babies'/>
                                 
 
                 </div>
                 <div className='col-3 position-relative img_mask'>
-                    <p className='img_description top-50 start-50 translate-middle fw-bold' >babies</p>
+                    <p className='img_text top-50 start-50 translate-middle fw-bold' >babies</p>
                     <img src={'https://images.pexels.com/photos/4964290/pexels-photo-4964290.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'} className='p-1 w-100 img_img' alt='babies'/>
                                 
 
                 </div>
                 <div className='col-3 position-relative img_mask'>
-                    <p className='img_description top-50 start-50 translate-middle fw-bold' >babies</p>
+                    <p className='img_text top-50 start-50 translate-middle fw-bold' >babies</p>
                     <img src={'https://images.pexels.com/photos/4964290/pexels-photo-4964290.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'} className='p-1 w-100 img_img' alt='babies'/>
                                 
 
                 </div>
                 <div className='col-3 position-relative img_mask'>
-                    <p className='img_description top-50 start-50 translate-middle fw-bold' >babies</p>
+                    <p className='img_text top-50 start-50 translate-middle fw-bold' >babies</p>
                     <img src={'https://images.pexels.com/photos/4964290/pexels-photo-4964290.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'} className='p-1 w-100 img_img' alt='babies'/>
                                 
 
                 </div>
                 <div className='col-3 position-relative img_mask'>
-                    <p className='img_description top-50 start-50 translate-middle fw-bold' >babies</p>
+                    <p className='img_text top-50 start-50 translate-middle fw-bold' >babies</p>
                     <img src={'https://images.pexels.com/photos/4964290/pexels-photo-4964290.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'} className='p-1 w-100 img_img' alt='babies'/>
                                 
 
                 </div>
                 <div className='col-3 position-relative img_mask'>
-                    <p className='img_description top-50 start-50 translate-middle fw-bold' >babies</p>
+                    <p className='img_text top-50 start-50 translate-middle fw-bold' >babies</p>
                     <img src={'https://images.pexels.com/photos/4964290/pexels-photo-4964290.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'} className='p-1 w-100 img_img' alt='babies'/>
                                 
 
                 </div>
                 <div className='col-3 position-relative img_mask'>
-                    <p className='img_description top-50 start-50 translate-middle fw-bold' >babies</p>
+                    <p className='img_text top-50 start-50 translate-middle fw-bold' >babies</p>
                     <img src={'https://images.pexels.com/photos/4964290/pexels-photo-4964290.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'} className='p-1 w-100 img_img' alt='babies'/>
                                 
 
                 </div>
                 <div className='col-3 position-relative img_mask'>
-                    <p className='img_description top-50 start-50 translate-middle fw-bold' >babies</p>
+                    <p className='img_text top-50 start-50 translate-middle fw-bold' >babies</p>
                     <img src={'https://images.pexels.com/photos/4964290/pexels-photo-4964290.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'} className='p-1 w-100 img_img' alt='babies'/>
                                 
 
